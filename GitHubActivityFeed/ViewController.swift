1a4cd68e5e629cd6e021f7fad0347a7ef88fbbc1
lyzkov <lyzkov@gmail.com>
2019-06-26 18:54:37 +0200

    Initial Commit

---
+++ GitHubActivityFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubActivityFeed
+//
+//  Created by BOGU$ on 26/06/2019.
+//  Copyright © 2019 lyzkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
