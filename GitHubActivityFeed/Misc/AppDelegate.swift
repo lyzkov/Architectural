6d3d87a39a53bc5c16c9324686cb84ad9d6ca9d6
lyzkov <lyzkov@gmail.com>
2019-06-26 19:40:56 +0200

    Project init

---
+++ GitHubActivityFeed/Misc/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GitHubActivityFeed
+//
+//  Created by BOGU$ on 26/06/2019.
+//  Copyright © 2019 lyzkov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
